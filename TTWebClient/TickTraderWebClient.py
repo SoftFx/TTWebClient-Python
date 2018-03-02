6dd333a121248dbcbe7baa7b6d94982361a7e7b4
Ivan Shynkarenka <chronoxor@gmail.com>
2018-03-02 16:28:01 +0300

    Daily account snapshots

--- TTWebClient/TickTraderWebClient.py
+++ TTWebClient/TickTraderWebClient.py
@@ -507,7 +507,10 @@ class TickTraderWebClient:
         New trade history request is described by the filling following fields:
         - **TimestampFrom** (optional) - Lower timestamp bound of the trade history request
         - **TimestampTo** (optional) - Upper timestamp bound of the trade history request
+        - **OrderId** (optional) - Skip canel order history records
+        - **SkipCancelOrder** (optional) - OrderId to filter the trade history request
         - **RequestDirection** (optional) - Request paging direction ("Forward" or "Backward"). Default is "Forward".
+        - **RequestPageSize** (optional) - Request page size. Default is 100.
         - **RequestFromId** (optional) - Request paging from Id
 
         If timestamps fields are not set trade history will be requests from the begin or from the current timestamp
@@ -531,32 +534,32 @@ class TickTraderWebClient:
         response = json.loads(self.__decode_response(client.getresponse()))
         return response
 
-    def get_trade_history_by_id(self, trade_id, request):
+    def get_daily_snapshots(self, request):
         """
-        Get account trade history for the given trade Id
+        Get daily account snapshots
 
-        New trade history request is described by the filling following fields:
-        - **TimestampFrom** (optional) - Lower timestamp bound of the trade history request
-        - **TimestampTo** (optional) - Upper timestamp bound of the trade history request
+        New daily account snapshots request is described by the filling following fields:
+        - **TimestampFrom** (optional) - Lower timestamp bound of the daily account snapshot request
+        - **TimestampTo** (optional) - Upper timestamp bound of the daily account snapshot request
         - **RequestDirection** (optional) - Request paging direction ("Forward" or "Backward"). Default is "Forward".
+        - **RequestPageSize** (optional) - Request page size. Default is 100.
         - **RequestFromId** (optional) - Request paging from Id
 
-        If timestamps fields are not set trade history will be requests from the begin or from the current timestamp
+        If timestamps fields are not set daily account snapshot will be requests from the begin or from the current timestamp
         depending on **RequestDirection** value.
 
-        Trade history is returned by chunks by paging size (default is 100). You can provide timestamp bounds (from, to)
-        and direction of access (forward or backward). After the first request you'll get a list of trade history
-        records with Ids. The next request should contain **RequestFromId** with the Id of the last processed trade
-        history record. As the result you'll get the next chunk of trade history records. If the last page was reached
+        Snapshots are returned by chunks by paging size (default is 100). You can provide timestamp bounds (from, to)
+        and direction of access (forward or backward). After the first request you'll get a list of daily account snapshots
+        records with Ids. The next request should contain **RequestFromId** with the Id of the last processed snapshot
+        record. As the result you'll get the next chunk of daily account snapshots records. If the last page was reached
         response flag **IsLastReport** will be set.
 
         Keyword arguments:
-        trade_id -- trade Id
-        request -- trade history request
+        request -- daily account snapshots request
         """
         client = self.__create_http_client()
         method = 'POST'
-        url_relative = '/api/v1/tradehistory/{0}'.format(trade_id)
+        url_relative = '/api/v1/dailysnapshots'
         url_absolute = 'https://{0}{1}'.format(self.__web_api_address, url_relative)
         body = json.dumps(request)
         client.request(method, url_relative, body.encode(), self.__get_http_hmac_headers(method, url_absolute, body))
